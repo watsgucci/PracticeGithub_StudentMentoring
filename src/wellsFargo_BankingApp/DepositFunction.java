6aaed408cb1aa7091898036013005bb8d4bd33ec
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-12-23 22:20:55 -0500

    Added a comment to DepositFunction.java

--- src/wellsFargo_BankingApp/DepositFunction.java
+++ src/wellsFargo_BankingApp/DepositFunction.java
@@ -4,7 +4,7 @@ public class DepositFunction {
     public static void main(String[] args) {
 
         System.out.println("Day 1, Knowledge Transfer from my coWorkers");
-
+        //came back from lunch and added a comment
 
     }
 
