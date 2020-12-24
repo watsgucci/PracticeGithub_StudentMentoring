1be21bed19cabb232079900bad16c9e5bf71a963
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-12-23 22:25:14 -0500

    Developer did some work

--- src/wellsFargo_BankingApp/DepositFunction.java
+++ src/wellsFargo_BankingApp/DepositFunction.java
@@ -1,10 +1,18 @@
 package wellsFargo_BankingApp;
 
+import java.sql.SQLOutput;
+
 public class DepositFunction {
     public static void main(String[] args) {
 
         System.out.println("Day 1, Knowledge Transfer from my coWorkers");
         //came back from lunch and added a comment
+
+        //************DEVELOPER DOING WORK********************
+        System.out.println("I am a developer! DO NOT TOUCH MY CODE!");
+        if(true){
+            System.out.println("Developers are the best");
+        }
 
     }
 
