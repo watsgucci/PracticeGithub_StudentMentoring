31c60872f05156ea9704db6b41a724f721b2e6a8
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-12-23 22:12:04 -0500

    Created DepositFunction.java Added a System.Out line marking Day 1

---
+++ src/wellsFargo_BankingApp/DepositFunction.java
@@ -0,0 +1,11 @@
+package wellsFargo_BankingApp;
+
+public class DepositFunction {
+    public static void main(String[] args) {
+
+        System.out.println("Day 1, Knowledge Transfer from my coWorkers");
+
+
+    }
+
+}
