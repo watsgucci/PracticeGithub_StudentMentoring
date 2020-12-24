5ee77346095793cc76c0f4bb22964feb5fbc81af
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-12-23 22:51:58 -0500

    added a sout to DepositFunction.java

--- src/wellsFargo_BankingApp/DepositFunction.java
+++ src/wellsFargo_BankingApp/DepositFunction.java
@@ -21,8 +21,11 @@ public class DepositFunction {
         System.out.println("Creating new feature DAy2 of work");
         System.out.println("Code is working great!");
 
+
+
         //I am a tester and I am starting to work on the miniFeature
         System.out.println("Mini Feature in progress");
+        System.out.println("Adding a comment because BlahBlah told me to");
 
 
     }
